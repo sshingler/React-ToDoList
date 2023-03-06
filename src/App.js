69bce94e8fd50dfc9e5c4ee9c42fa828f6ee3923
SShingler <s.shingler@hotmail.co.uk>
2023-03-06 16:39:55 +0000

    3 - add item to ToDoList

--- src/App.js
+++ src/App.js
@@ -3,32 +3,60 @@ import React, {useState} from 'react';
 
 function App() {
 
+  //declare initial list of objects
   const [items, setItems] = useState ([
     {name: "Buy shopping", priority:"high"}, 
     {name: "Clean Bathroom", priority:"low"},
     {name: "Car's MOT", priority:"high"},
-  ])
+  ]);
 
+  const [newItemName, setNewItemName] = useState (""); 
+
+  //render items 
   const itemNodes = items.map ((item, index) => {
     return (
       <li key = {index}>
         <span>{item.name}</span>
-
       </li>
     )
   })
+
+  function handleInputChange(event) {
+    setNewItemName(event.target.value);
+  }
+
+  function saveNewItem(event) {
+    event.preventDefault();
+    const newItem = {
+      name: newItemName,
+      priority: "low", 
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setNewItemName(""); 
+
+  }
+
+
 
 
   return (
     <div className="App">
     <h1>ToDo's</h1>  
     <hr></hr>
-    <ul>
-      {itemNodes}
+    <ul> 
+      {itemNodes}      
     </ul>
 
+    <form onSubmit={saveNewItem}>
+    <label htmlFor = "item-name">Add a new task:</label>
+    <input id="item-name" type="text" value={newItemName} onChange={handleInputChange} />
+    <input type="submit" value="Save New Task"/>  
 
-    
+    </form>
+
+
+
     </div>
   );
 }
