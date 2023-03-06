a4fea5c907d845e46b4aa211c2f9423fe36e4233
SShingler <s.shingler@hotmail.co.uk>
2023-03-06 16:16:27 +0000

    2 - UI displays items

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import React, {useState} from 'react';
 
 function App() {
 
+  const [items, setItems] = useState ([
+    {name: "Buy shopping", priority:"high"}, 
+    {name: "Clean Bathroom", priority:"low"},
+    {name: "Car's MOT", priority:"high"},
+  ])
 
+  const itemNodes = items.map ((item, index) => {
+    return (
+      <li key = {index}>
+        <span>{item.name}</span>
 
+      </li>
+    )
+  })
 
 
   return (
     <div className="App">
     <h1>ToDo's</h1>  
+    <hr></hr>
+    <ul>
+      {itemNodes}
+    </ul>
+
+
+    
     </div>
   );
 }
